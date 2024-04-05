f5d711f0ba6ee5eb46a3d123c61cd0a0eaf9d5e6
Moses Onche <onchemoses38@gmail.com>
2024-04-05 13:40:44 +0100

    Exercise: write a program that yields the sum of two numbers.

---
+++ cc-javascript-mar/week-1/exercises.js
@@ -0,0 +1,12 @@
+// INPUT
+let firstNumber = prompt("Type in the first number:");
+firstNumber = Number(firstNumber);
+
+let secondNumber = prompt("Type in the second number");
+secondNumber = Number(secondNumber);
+
+// PROCESSING
+let answer = firstNumber + secondNumber;
+
+// OUTPUT
+alert(firstNumber + " + " + secondNumber + " = " + answer);
