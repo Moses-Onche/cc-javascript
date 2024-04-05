43ce24e75e452c062ef890d7cc1646cd10ebff24
Moses Onche <onchemoses38@gmail.com>
2024-04-05 12:17:33 +0100

    Use the sort method to rearrange an array

--- cc-javascript-mar/week-1/array.js
+++ cc-javascript-mar/week-1/array.js
@@ -5,4 +5,10 @@ cars.push("Lexus");
 cars.push("Mazda");
 
 cars.splice(2, 0, "Fiat");
-console.log(cars.join("\n"));
+//console.log(cars.join("\n"));
+
+// sort() method
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.sort());
+const evenNumbers = [6, 4, 2, 10, 8];
+console.log(evenNumbers.sort());
