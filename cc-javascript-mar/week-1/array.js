9d4335bc1ded54d6f3d006b85d0abd811737bc1b
Moses Onche <onchemoses38@gmail.com>
2024-04-05 13:28:44 +0100

    Update on how to use objects

--- cc-javascript-mar/week-1/array.js
+++ cc-javascript-mar/week-1/array.js
@@ -12,3 +12,10 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.sort());
 const evenNumbers = [6, 4, 2, 10, 8];
 console.log(evenNumbers.sort());
+
+// reverse() method
+console.log(fruits.reverse());
+console.log(evenNumbers.reverse());
+
+document.write("<h1>This is Javascript</h1>");
+document.write("<h2>" + newFruits +"</h2>");
---
+++ cc-javascript-mar/week-1/objects.js
@@ -0,0 +1,15 @@
+const car = {
+  brand: "Honda",
+  type: "SUV",
+  model: "Crosstour",
+  cylinder: 6,
+  isFairlyUsed: true,
+  color: "white"
+}
+
+document.write("<p>" + car.type + "</p>");
+let score = 64;
+let grade;
+
+score > 70 ? grade = "A" : grade = "B";
+document.write(grade);
