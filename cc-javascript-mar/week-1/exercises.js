974e09c1461b012b06190e9bb2eae07b8d89a0ac
Moses Onche <onchemoses38@gmail.com>
2024-04-08 12:46:58 +0100

    Write a program that returns the first element of an array

--- cc-javascript-mar/week-1/exercises.js
+++ cc-javascript-mar/week-1/exercises.js
@@ -1,12 +1,27 @@
-// INPUT
+// The sum of two numbers
+/*
 let firstNumber = prompt("Type in the first number:");
 firstNumber = Number(firstNumber);
-
 let secondNumber = prompt("Type in the second number");
 secondNumber = Number(secondNumber);
 
-// PROCESSING
 let answer = firstNumber + secondNumber;
 
-// OUTPUT
 alert(firstNumber + " + " + secondNumber + " = " + answer);
+*/
+
+// Convert minutes to seconds
+/*
+let minutes = 5;
+
+let answer = minutes * 60;
+
+alert(minutes + " minutes is equal to " + answer + " seconds.");
+*/
+
+// let hour = 7;
+
+// alert(hour + " hour")
+
+// let arrayOfNumbers = [2, 4, 6, 8, 10];
+// console.log(arrayOfNumbers[0]);
