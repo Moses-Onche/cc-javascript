25e4496d39caebadffc3f09cbc9e407e647fa7cf
Moses Onche <onchemoses38@gmail.com>
2024-04-12 13:52:59 +0100

    Using for and while loops

--- cc-javascript-mar/week-1/exercises.js
+++ cc-javascript-mar/week-1/exercises.js
@@ -1,40 +1,94 @@
 // The sum of two numbers
-/*
-let firstNumber = prompt("Type in the first number:");
-firstNumber = Number(firstNumber);
-let secondNumber = prompt("Type in the second number");
-secondNumber = Number(secondNumber);
 
-let answer = firstNumber + secondNumber;
+// let firstNumber = prompt("Type in the first number:");
 
-alert(firstNumber + " + " + secondNumber + " = " + answer);
-*/
+// firstNumber = Number(firstNumber);
+// alert(typeof firstNumber);
+// let secondNumber = prompt("Type in the second number");
+// secondNumber = Number(secondNumber);
+
+// let answer = firstNumber + secondNumber;
+
+// alert(firstNumber + " + " + secondNumber + " = " + answer);
+
 
 // Convert minutes to seconds
-/*
-let minutes = 5;
 
-let answer = minutes * 60;
+// let minutes = 5;
 
-alert(minutes + " minutes is equal to " + answer + " seconds.");
-*/
+// let answer = minutes * 60;
+
+// alert(minutes + " minutes is equal to " + answer + " seconds.");
+
 
 // let hour = 7;
 
 // alert(hour + " hour")
 
 // let arrayOfNumbers = [2, 4, 6, 8, 10];
-// console.log(arrayOfNumbers[0]);
+// alert(arrayOfNumbers[0]);
 
 // let finalMessage = "something" + " ";
 // finalMessage = finalMessage + "is better than nothing."
 // console.log(finalMessage);
 
-let age = prompt("Type in your age");
-age = Number(age);
+// let age = prompt("Type in your age");
+// age = Number(age);
 
-if (age >= 18 ){
-  alert("You are eligible to vote.");
-} else {
-  alert("You are too young");
-}
+// if (age >= 18 ){
+//   alert("You are eligible to vote.");
+// } else {
+//   alert("You are too young");
+// }
+
+// let score = 65;
+
+// if (score >= 70){
+//   console.log("Your grade: A");
+// } else if (score >= 60){
+//   console.log("Your grade: B");
+// } else if (score >= 50){
+//   console.log("Your grade: C");
+// } else {
+//   console.log("Your grade: D");
+// }
+
+// let score = prompt("Type in your score");
+
+// if (score >= 70){
+//   alert("Your grade: A");
+// } else if (score >= 60){
+//   alert("Your grade: B");
+// } else if (score >= 50){
+//   alert("Your grade: C");
+// } else {
+//   alert("Your grade: D");
+// }
+
+let numArray = [];
+let input;
+
+// for (let count = 0; count < 5; count++) {
+//   input = prompt("Type in a number:");
+//   numArray.push(input);
+// }
+
+// let count = 0;
+
+// while (count < 5){
+//   input = prompt("Type in a number:");
+//   numArray.push(input);
+//   count++;
+// }
+// let exitProgram = true;
+
+// while (exitProgram){
+//   input = prompt("Type in a number.\nType 'stop' to end the program.");
+//   if (input === 'stop' || input === "Stop"){
+//     exitProgram = false;
+//   } else {
+//     numArray.push(input);
+//   }
+// }
+
+// alert(numArray);
