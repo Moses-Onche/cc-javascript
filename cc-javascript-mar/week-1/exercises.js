ea5270b7257f3bb57c1e717ccebcd088407f56ff
Moses Onche <onchemoses38@gmail.com>
2024-04-12 15:14:18 +0100

    Write a program to find the smallest and largest numbers in an array

--- cc-javascript-mar/week-1/exercises.js
+++ cc-javascript-mar/week-1/exercises.js
@@ -101,3 +101,23 @@ let input;
 // }
 
 // console.log(answer);
+
+let newArray = [3, 5, 1, 7, 1000, 9, 2, 10, 43, 4, 5, 2, 6, 10, 23, 12034];
+// let lowestNumber = 5;
+// let highestNumber = 0;
+
+// for (let count = 0; count <= newArray.length; count++){
+//   if (newArray[count] < newArray[count + 1] && newArray[count] < lowestNumber){
+//     lowestNumber = newArray[count];
+//   }
+//   if (newArray[count] > newArray[count + 1] && newArray[count] > highestNumber){
+//     highestNumber = newArray[count];
+//   }
+// }
+
+// console.log(lowestNumber);
+// console.log(highestNumber);
+
+let arranged = newArray.sort(function(a, b){return a-b});
+console.log(arranged[0]);
+console.log(arranged[arranged.length-1]);
