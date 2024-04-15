78ceb7ca1258d48062004bae4096b82ca34a3183
Moses Onche <onchemoses38@gmail.com>
2024-04-15 13:46:04 +0100

    Week 3 Basics of a function

---
+++ cc-javascript-mar/week-3/functions.js
@@ -0,0 +1,23 @@
+function myGreeting(message){
+  return console.log(message);
+}
+
+myGreeting("Hello");
+myGreeting("Today is Monday!");
+
+function addNumbers(firstNumber, secondNumber){
+  return console.log(firstNumber + secondNumber);
+}
+
+addNumbers(3, 4);
+
+let number = 5;
+function increase(){
+  let anotherNumber = 2;
+  return anotherNumber;
+}
+
+// anotherNumber + 1;
+
+console.log(increase());
+// console.log(anotherNumber);
