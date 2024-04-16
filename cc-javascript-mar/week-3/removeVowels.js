bb9c284a628fccc184bd2ddd6577476491c3372d
Moses Onche <onchemoses38@gmail.com>
2024-04-16 13:56:43 +0100

    Write a program that removes the vowels from a string and pushes the consonants to a new string

---
+++ cc-javascript-mar/week-3/removeVowels.js
@@ -0,0 +1,13 @@
+function removeVowels(text){
+  let newString = "";
+
+  for (let count = 0; count <= text.length - 1; count++){
+    if (text[count] != "a" && text[count] != "e" && text[count] != "i" &&
+     text[count] != "o" && text[count] != "u"){
+      newString += text[count];
+    }
+  }
+  console.log(newString);
+}
+
+removeVowels("I have never seen a thin person drinking Diet Coke.");
