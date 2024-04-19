6865af363cea37efc825bb8b8a42bfb1057a00d8
Moses Onche <onchemoses38@gmail.com>
2024-04-19 12:58:46 +0100

    Working time model using javascript

--- cc-javascript-mar/week-3/date.js
+++ cc-javascript-mar/week-3/date.js
@@ -1,12 +1,31 @@
-function timeForMilkAndCookies(date){
-  // console.log(date.getMonth());
-  // console.log(date.getDate());
+// function timeForMilkAndCookies(date){
+//   // console.log(date.getMonth());
+//   // console.log(date.getDate());
 
-  if (date.getMonth() === 11 && date.getDate() === 24){
-    return true;
-  } else {
-    return false;
-  }
-}
+//   if (date.getMonth() === 11 && date.getDate() === 24){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-console.log(timeForMilkAndCookies(new Date(2013, 0, 23)));
+// console.log(timeForMilkAndCookies(new Date(2013, 0, 23)));
+
+
+
+let switcher = true;
+setInterval(() => {
+  let currentDate = new Date();
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+  // if (switcher) {
+  //   document.getElementById('date').innerHTML = time.toString();
+  //   switcher = false;
+  // } else {
+  //   document.getElementById('date').innerHTML = time.toString();
+  //   switcher = true;
+  // }
+  document.getElementById('date').innerHTML = time;
+}, 1000)
+
+// setTimeout(function, 0);
+// () => {}
