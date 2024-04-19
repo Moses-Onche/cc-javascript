f3adc08b901fc6fa6c8f810d6ace733a59889c5b
Moses Onche <onchemoses38@gmail.com>
2024-04-19 10:13:24 +0100

    Using tag name to select elements

--- cc-javascript-mar/week-3/dom.js
+++ cc-javascript-mar/week-3/dom.js
@@ -1,1 +1,6 @@
-console.log(document.documentElement);
+// console.log(document.documentElement);
+let paragraphTags = document.getElementsByTagName("p");
+console.log(paragraphTags);
+paragraphTags[1].innerHTML = "Is not";
+// secondParagraph = "Is not"
+// console.log(secondParagraph);
