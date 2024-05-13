ea254f6718924b09ac075e6c5c3ec7a1618a765f
Moses Onche <onchemoses38@gmail.com>
2024-05-13 13:49:47 +0100

    Write a program that grades students according to their score.

--- cc-javascript-apr/week-2/programStructure.js
+++ cc-javascript-apr/week-2/programStructure.js
@@ -7,12 +7,26 @@
 //   console.log("This number is odd");
 // }
 
-let num = Number(prompt("Type in a number: "));
+// let num = Number(prompt("Type in a number: "));
 
-if (num < 10) {
-  console.log("Small")
-} else if (num < 100) {
-  console.log("Medium")
+// if (num < 10) {
+//   console.log("Small")
+// } else if (num < 100) {
+//   console.log("Medium")
+// } else {
+//   console.log("Large")
+// }
+
+let input = Number(prompt("Type in your score:"))
+
+if (input > 70) {
+  console.log("Grade: A")
+} else if (input > 60) {
+  console.log("Grade: B")
+} else if (input > 50) {
+  console.log("Grade: C")
+} else if (input > 40) {
+  console.log("Grade: D")
 } else {
-  console.log("Large")
+  console.log("Grade: E")
 }
