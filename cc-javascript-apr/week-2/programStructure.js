82ae9134163feae43150d1138e3960648e4a712f
Moses Onche <onchemoses38@gmail.com>
2024-05-13 15:06:04 +0100

    Write a program that simulates the javascript function Math.pow

--- cc-javascript-apr/week-2/programStructure.js
+++ cc-javascript-apr/week-2/programStructure.js
@@ -42,9 +42,19 @@
 //   console.log("You are eligible to vote")
 // }
 
-let number = 0;
+// let number = 0;
 
-while (number <= 12) {
-  console.log(number);
-  number = number + 2;
+// while (number <= 12) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+let result = 1;
+let counter = 0;
+
+while (counter < 10){
+  result = result * 2; // 2 ^ 10 = 2 x 2 x 2...2
+  counter = counter + 1;
 }
+
+console.log(result);
