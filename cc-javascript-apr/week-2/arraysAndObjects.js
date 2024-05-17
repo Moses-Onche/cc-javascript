ee5df7a68c974cf771a18f60bb2a9373518abe9f
Moses Onche <onchemoses38@gmail.com>
2024-05-17 14:36:32 +0100

    Thursday and Friday class

---
+++ cc-javascript-apr/week-2/arraysAndObjects.js
@@ -0,0 +1,42 @@
+let listOfNumbers = [2, 3, 5, 7, 11];
+// console.log(listOfNumbers);
+
+// let firstName = 'Joseph';
+// console.log(firstName.indexOf('s'));
+console.log(Math.floor(Math.random() * 90))
+
+// for (let count = 0; count < listOfNumbers.length; count++){
+//   console.log(listOfNumbers[count] + 5)
+// }
+
+// for (let item of listOfNumbers){
+//   console.log(item);
+// }
+// console.log(listOfNumbers);
+// console.log(listOfNumbers[3]);
+// console.log(listOfNumbers.length);
+
+// let namesOfDistricts = ['Kubwa', 'Wuse', 'Maitama', 'Asokoro', 'Lugbe'];
+// console.log(namesOfDistricts)
+
+// let booleanValues = [true, false, false, true, false];
+// console.log(booleanValues)
+
+// let sequence = [1, 2, 3]
+// sequence.push(5)
+// sequence.push(4)
+// console.log(sequence);
+// console.log(sequence.pop())
+// console.log(sequence)
+
+// let chair = {
+//   chairType: 'banquet',
+//   foamColor: 'red',
+//   frameColor: 'blue',
+//   stopperColor: 'white',
+//   stopperQuantity: 4,
+//   legs: 4,
+//   chairComfortable: true,
+// }
+
+// console.log(Object.keys(chair));
---
+++ cc-javascript-apr/week-2/exercises.js
@@ -0,0 +1,11 @@
+function addition(num1){
+  return num1 + num2;
+}
+
+addition(2, 3)
+addition(8, 12);
+
+// num1: paramenter
+// num2: parameter
+// 2: argument
+// 3: argument
