fc0d811aa32c44d8b1e45156a488175e0df73f3f
Moses Onche <onchemoses38@gmail.com>
2024-05-23 13:27:18 +0100

    Use javascript to change CSS properties of HTML elements

--- cc-javascript-apr/week-3/dom.js
+++ cc-javascript-apr/week-3/dom.js
@@ -1,4 +1,12 @@
 let paragraphs = document.getElementsByTagName('p');
-for (let count = 0; count <= paragraphs.length; count++){
-  paragraphs[count].innerHTML = "new content";
-}
+let heading = document.getElementsByClassName('heading');
+let firstParagraph = document.getElementById('first');
+let link = document.getElementById('next-page');
+
+// for (let count = 0; count <= paragraphs.length; count++){
+//   paragraphs[count].innerHTML = "new content";
+// }
+
+// console.log(heading)
+link.href = 'https://www.google.com';
+firstParagraph.style.color = 'red';
