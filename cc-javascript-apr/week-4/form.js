106d85e891a79a71b1eb9b7b74febf5da2b133f4
Moses Onche <onchemoses38@gmail.com>
2024-05-27 13:29:07 +0100

    Use javascript to change form input

---
+++ cc-javascript-apr/week-4/form.js
@@ -0,0 +1,9 @@
+// document.querySelector('input').focus();
+// console.log(document.activeElement.tagName);
+
+// document.querySelector('input').blur();
+// console.log(document.activeElement.tagName);
+
+let form = document.getElementById('second-form');
+
+console.log(form[2]);
