145eaae7adb821c92438e7836b53b38e01410962
Moses Onche <onchemoses38@gmail.com>
2024-05-29 12:31:22 +0100

    Update notes code

--- cc-javascript-apr/week-4/notes.js
+++ cc-javascript-apr/week-4/notes.js
@@ -1,50 +1,42 @@
-let list = document.querySelectorAll('select');
-let note = document.querySelectorAll('textarea');
+let list = document.querySelector("select");
+let note = document.querySelector("textarea");
 
 let state;
-function setState(newState){
-  list.textContent = '';
-  for (let name of Object.keys(newState.notes)){
-    let option = document.createElement('option');
+function setState(newState) {
+  list.textContent = "";
+  for (let name of Object.keys(newState.notes)) {
+    let option = document.createElement("option");
     option.textContent = name;
-    if (newState.selected == name){
-      option.selected = true;
-    }
+    if (newState.selected == name) option.selected = true;
     list.appendChild(option);
   }
   note.value = newState.notes[newState.selected];
 
-  localStorage.setItem('Notes', JSON.stringify(newState));
+  localStorage.setItem("Notes", JSON.stringify(newState));
   state = newState;
 }
-setState(JSON.parse(localStorage.getItem("Notes")) ?? {
-  notes: {'shopping lists': 'Carrots\nRaisins'},
-  selected: 'shopping lists'
+setState(
+  JSON.parse(localStorage.getItem("Notes")) ?? {
+    notes: { "shopping list": "Carrots\nRaisins" },
+    selected: "shopping list",
+  }
+);
+
+list.addEventListener("change", () => {
+  setState({ notes: state.notes, selected: list.value });
 });
-
-list.addEventListener('change', () => {
-  let {selected} = state;
+note.addEventListener("change", () => {
+  let { selected } = state;
   setState({
-    notes: {...state.notes, [selected]: note.value},
-    selected
-  })
-})
-
-document.querySelector('button')
-  .addEventListener('click', () => {
-    let name = prompt("Note name:");
-    if (name) setState({
-      notes: {...state.notes, [name]:""},
-      selected: name
-    })
-  })
-
-// let button = document.getElementById('new');
-
-// button.addEventListener('click', function(){
-//   let newParagraph = document.createElement('p');
-//   newParagraph.textContent = "Nigeria is the largest country in Africa by population";
-//   // document.body.appendChild(newParagraph);
-//   console.log(firstButton)
-//   firstButton.appendChild(newParagraph);
-// })
+    notes: { ...state.notes, [selected]: note.value },
+    selected,
+  });
+});
+document.querySelector("button").addEventListener("click", () => {
+  let name = prompt("Note name");
+  if (name)
+    setState({
+      notes: { ...state.notes, [name]: "" },
+      selected: name,
+    });
+});
