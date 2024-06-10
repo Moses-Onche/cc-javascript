25c5d4bf08498e2f9a09693075add547a89ab28b
Moses Onche <onchemoses38@gmail.com>
2024-06-10 13:39:18 +0100

    Create and update objects

--- cc-javascript-may/week2/objects.js
+++ cc-javascript-may/week2/objects.js
@@ -3,3 +3,17 @@
 // const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
 // console.log(date.getUTCMonth());
 
+const table = {
+  noOfLegs: 4,
+  color: 'white',
+  material: 'plastic',
+  isFoldable: true,
+  height: 0.7,
+}
+
+console.log(table.isFoldable);
+
+table['colorOfLegs'] = 'grey';
+table['width'] = 1.2;
+
+console.log(table.colorOfLegs + ' ' + table.material + ' ' + table.width)
