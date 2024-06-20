fd7ab0cf7ff66d3a9e94d2c6d2012e9213644d7b
Moses Onche <onchemoses38@gmail.com>
2024-06-20 11:51:12 +0100

    Learn about the DOM and apply its concepts to HTML and CSS

---
+++ cc-javascript-may/week3/dom.js
@@ -0,0 +1,18 @@
+// let heading = document.getElementById('main-heading').innerHTML = 'New topic';
+// heading.innerHTML = 'New topic';
+
+let paragraphs = document.getElementsByTagName('p');
+let sub = document.getElementsByClassName('subheading');
+
+sub[1].style.fontSize = '90px';
+
+// for (let i = 0; i < paragraphs.length; i++){
+//   paragraphs[i].innerHTML = 'I sabi Javascript';
+//   // if (i === paragraphs.length - 1){
+//   //   paragraphs[i].innerHTML = 'This thing had sha'
+//   // } else {
+//   //   paragraphs[i].innerHTML = 'I sabi Javascript';
+//   // }
+// }
+
+
