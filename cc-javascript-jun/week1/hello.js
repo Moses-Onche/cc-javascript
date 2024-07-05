21b00afd4c407013223af88efb3d2dcf70b26eeb
Moses Onche <onchemoses38@gmail.com>
2024-07-05 11:47:26 +0100

    More operators, conditionals and concatenation

---
+++ cc-javascript-jun/week1/hello.js
@@ -0,0 +1,8 @@
+// let input = Number(prompt('Type in the money paid:'))
+// let item = Number(prompt('Type in the price of the item:'));
+
+// alert(input - item);
+
+let name = prompt('Type in your name:');
+
+alert(`Hello, ` + name);
