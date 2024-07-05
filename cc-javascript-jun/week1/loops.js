538c3c0c0fe70d2dd033fa88fb543bb5b18fa876
Moses Onche <onchemoses38@gmail.com>
2024-07-05 13:19:53 +0100

    Learn about for loops and while loops

---
+++ cc-javascript-jun/week1/loops.js
@@ -0,0 +1,21 @@
+// for (let count = 2; count <= 3; count = count + 2){
+//   console.log('Hello');
+// }
+
+// for (count = 10; count >= 1; count = count - 1){
+//   console.log(count);
+//   // console.log('Happy New Year!');
+// }
+// console.log('Happy New Year!');
+
+// let count = 1;
+
+// while (count < 10){
+//   console.log('This is a while loop');
+//   count = count + 1;
+// }
+
+while (true){
+  console.log('Infinite loop');
+}
+
