f19bfa18142e045f7344c2e8324cac9dea2e3563
Moses Onche <onchemoses38@gmail.com>
2024-07-09 14:45:48 +0100

    Create assignment for object class

---
+++ cc-javascript-jun/week2/objects.js
@@ -0,0 +1,33 @@
+const cars = {};
+
+cars['model'] = 'Tesla';
+cars['doors'] = 4;
+cars['is Electric'] = true;
+cars['color'] = 'red';
+
+console.log(cars);
+console.log(cars.model);
+console.log(cars.doors);
+console.log(cars['is Electric']);
+
+// Assignment: Pick a real world object in your surrounding
+// and build it in javascript code. Write at least 10 properties
+// about the object.
+
+const greenDuster = {
+  color: 'green',
+  isMagnetic: true,
+  wiperColor: 'black',
+  grip: 'lines',
+  size: 'medium',
+  material: 'plastic',
+  cleaningSurface: 'flat',
+  quantity: 3,
+  reusable: true,
+  disposable: 'false',
+  texture: 'smooth',
+  shape: 'rounded',
+  condition: 'new',
+  cleanable: true,
+  
+}
