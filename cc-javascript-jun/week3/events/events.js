60aa8ee6166878a7cb4da9dd215e0d3c8d59a874
Moses Onche <onchemoses38@gmail.com>
2024-07-19 13:29:13 +0100

    Use onload event to load a subscription box using Javascript.

--- cc-javascript-jun/week3/events/events.js
+++ cc-javascript-jun/week3/events/events.js
@@ -10,9 +10,14 @@
 
 // document.body.onload = function(){ alert('When a page loads') };
 
-// document.body.onload = setTimeout()
-function sayHello(){
-  console.log('Hello');
+document.body.onload = setTimeout(displaySubscriptionBox, 3000);
+
+function displaySubscriptionBox(){
+  let subContainer = document.getElementsByClassName('sub-container');
+  subContainer[0].style.display = 'block';
 }
 
-setTimeout(sayHello, 5000);
+// displaySubscriptionBox();
+
+// document.body.onunload = alert('Are you sure you want to leave?');
+
