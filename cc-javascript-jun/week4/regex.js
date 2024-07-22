f8f9a7104b114645f95d9f19c8110db42cca6b68
Moses Onche <onchemoses38@gmail.com>
2024-07-22 15:12:06 +0100

    Learn about regular expressions

--- cc-javascript-jun/week4/regex.js
+++ cc-javascript-jun/week4/regex.js
@@ -1,1 +1,11 @@
-let 
+let song = `Sometimes all I think about is you
+Late nights in the middle of June
+Heat waves been faking me out
+Can't make you happier now`;
+
+// console.log(song.search(/you/ig));
+let newLyrics = song.replace(/You/gi, 'Tems');
+// console.log(newLyrics);
+// console.log(song[74], song[75], song[76], song[77], song[78]);
+// console.log(song.search(/\s/ig));
+console.log(/you/ig.exec(song));
