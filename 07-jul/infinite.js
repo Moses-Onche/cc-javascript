363bc196748faea0c24a97d0baf129afba411261
Moses Onche <onchemoses38@gmail.com>
2024-08-19 12:58:20 +0100

    Speed-build: Udacity login page

--- 07-jul/infinite.js
+++ 07-jul/infinite.js
@@ -1,10 +1,11 @@
 let magicNumber = 8;
 
 while (true){
-  let input = Number(prompt(`Guess and type in any number from 1 to 10\n(Type 'close' to exit)`));
-  if (input === 'close'){
+  let input = prompt(`Guess and type in any number from 1 to 10\n(Type 'close' to exit)`);
+  if (input === 'close' || input === 'Close'){
     break;
   } else {
+    input = Number(input);
     if (input === magicNumber){
       alert('Correct, ' + input + ' is the magic number');
       break;
