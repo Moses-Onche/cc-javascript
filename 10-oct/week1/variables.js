d90c10bbb7fc01b27979efffed127790165f85da
Moses-Onche <onchemoses38@gmail.com>
2024-10-08 14:20:38 +0100

    Learn about Variables and Operators

---
+++ 10-oct/week1/variables.js
@@ -0,0 +1,54 @@
+// alert('hello');
+// This is 
+// a
+// multiline
+// comment
+/*
+This is
+a multiline
+comment
+*/
+
+let fname = 'Moses';
+// alert(fname);
+fname = 'First place';
+// alert(fname);
+
+const surname = 'Adebayo';
+// alert(surname);
+// surname = 'Musa';
+// alert(surname);
+
+let firstNum = 5;
+let secondNum = 2;
+let add = firstNum + secondNum;
+console.log('5 + 2 = ' + add);
+let subtract = firstNum - secondNum;
+let divide = firstNum / secondNum;
+let multiply = firstNum * secondNum;
+console.log('5 - 2 = ' + subtract);
+console.log('5 / 2 = ' + divide);
+console.log('5 * 2 = ' + multiply);
+let power = firstNum ** secondNum;
+console.log('5 raised to power 2 = ' + power);
+let remainder = firstNum % secondNum;
+console.log('5 divided by 2 is 2 r ' + remainder);
+console.log('5 > 2 = ' + (firstNum > secondNum));
+console.log('5 < 2 = ' + (firstNum < secondNum));
+console.log('5 >= 2 = ' + (firstNum >= secondNum));
+console.log('5 <= 2 = ' + (firstNum <= secondNum));
+console.log(' 5 is equal to 5 ' + (firstNum == firstNum));
+console.log('5' == 5);
+console.log('5' === 5);
+console.log('5 is not equal to 2 ' + (firstNum != secondNum));
+
+let colorOfTop = 'white';
+let colorOfLegs = 'ash';
+let numberOfLegs = 4;
+let length = 1.5;
+let isFoldable = true;
+let materialOfTop = 'plastic';
+let materialOfLegs = 'aluminium';
+let isHeavy = false;
+let texture = 'coarse';
+let noOfTables = 6;
