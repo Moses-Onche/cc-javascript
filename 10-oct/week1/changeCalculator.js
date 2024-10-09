59256772334e16d36907f537bef10794fb51d8e3
Moses-Onche <onchemoses38@gmail.com>
2024-10-09 14:41:23 +0100

    Build a simple webpage and learn about strings

---
+++ 10-oct/week1/changeCalculator.js
@@ -0,0 +1,12 @@
+// INPUT
+let costOfItem = prompt('Type in the price of the item:');
+let amountPaid = prompt('Type in the amount paid:');
+
+// PROCESSING
+let change = amountPaid - costOfItem;
+
+// OUTPUT
+alert('Your change is N' + change);
+
+// let input = prompt('Type in your name');
+// alert(input);
---
+++ 10-oct/week1/strings.js
@@ -0,0 +1,13 @@
+let sentence = `He's often called "Johnny"`;
+console.log(sentence);
+
+let songTitles = `
+Roma
+Blessed
+Essence
+Anoti
+Mood
+Grace
+Mighty Whine
+Long Time
+`;
